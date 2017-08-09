1e9fa4d24fed5ae359b080ff6f732a304b8b2d8b
zakbain <zakbain@gmail.com>
2017-08-09 13:00:32 -0400

    Fixed formatting of HH:MM

--- SimpleTime.rb
+++ SimpleTime.rb
@@ -12,7 +12,25 @@ class SimpleTime
   
   # Print time as "HH:MM"
   def to_s
-    hours.to_s + ":" + minutes.to_s
+    # used to store hours and minutes as two digits (e.g.  7:5 => 07:05)
+    two_digit_hours = ""
+    two_digit_minutes = ""
+    
+    # if hours are less than 10, append a 0 to make two digits
+    if (hours < 10)
+      two_digit_hours = "0" + hours.to_s
+    else
+      two_digit_hours = hours.to_s
+    end
+    
+    # if minutes are less than 10, append a 0 to make two digits
+    if (minutes < 10)
+      two_digit_minutes = "0" + minutes.to_s
+    else
+      two_digit_minutes = minutes.to_s
+    end
+    
+    two_digit_hours + ":" + two_digit_minutes
   end
   
   # Make start and end time readable
